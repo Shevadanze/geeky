badd1f2a3d0459624d343d07777500586e96887d
Shevadanze <shevadanze@hotmail.com>
2025-07-30 21:23:07 +0300

    Data Structures

--- nexus.py
+++ nexus.py
@@ -1,4 +1,13 @@
-num1=24
-num2=5.3
+num1=24 #Integer
+num2=5.3 #Float
 print(num1)
 print(num2)
+languages=["Python", "Dart", "Web", 23] #List data type
+print(languages)
+print(languages[0]) #Access a list by index
+products=("Xbox", 450, "Mango", 20)  #tuple
+print(products[2])
+students_id={100,120,123,125} #set
+print(students_id)
+capital_city={"Kenya": "Nairobi", "Nigeria": "Lagos"}
+print(capital_city)
